9e107aeb75f3593d3a54dc16fa75b4da8f072296
Salah Eddine Lachkar <lachkar.se@gmail.com>
2020-02-21 01:42:23 +0100

    Add Stepper class

---
+++ js/main.js
@@ -0,0 +1,64 @@
+class Stepper {
+    #currentStep = 0;
+    
+    constructor({ id, steps }) {
+        this.stepperElement = document.getElementById(id);
+        this.stepperElement.className = "stepper";
+        this.addSteps(steps);
+    }
+
+    addSteps(steps) {
+        steps.forEach(({ title, description, fields }) => {
+            const template = `<div class="stepper__step">
+                <div class="stepper__title">${title}<div>
+                <div class="stepper__description">${description}<div>
+                <div class="fields">
+                    ${ fields
+                    .map(({
+                        type,
+                        placeholder
+                    }) => `<input type="${type}" placeholder="${placeholder}"/>`)
+                    .join``}
+                </div>
+            </div>`;
+            this.stepperElement.innerHTML += template;
+        });
+        this.showStep();
+    }
+
+    get currentStep() {
+        return this.#currentStep;
+    }
+
+    set currentStep(value) {
+        this.#currentStep = value;
+        this.showStep();
+    }
+
+    showStep() {
+        [...this.stepperElement.getElementsByClassName("stepper__step")].forEach((e, i) => {
+            e.className = "stepper__step" + (i === this.#currentStep ? "" : " stepper--hide");
+        });
+    }
+}
+
+const stepper = new Stepper({
+    id: "stepper",
+    steps: [
+        {
+            title: "Step1",
+            description: "Just a description",
+            fields: [
+                { type: "text", placeholder: "Your Email" }
+            ]
+        },
+        {
+            title: "Step2",
+            description: "Just a description 2",
+            fields: [
+                { type: "password", placeholder: "Your Password" },
+                { type: "password", placeholder: "Repeat Password" }
+            ]
+        }
+    ]
+});
