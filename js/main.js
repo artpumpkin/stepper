efe82e7b98ec1fd8da92a8f34ebdcdf18c6b73ce
Salah Eddine Lachkar <lachkar.se@gmail.com>
2020-02-23 00:19:08 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,96 +1,134 @@
 class Stepper {
-    #currentStep = 0;
-    
-    constructor({ id, steps }) {
-        this.numberOfSteps = steps.length;
-        this.stepperElement = document.getElementById(id);
-        this.stepperElement.className = "stepper";
-        this.stepperElement.innerHTML += `<div class="stepper__bullets">
-          ${`<div class="stepper__bullet"></div>`.repeat(steps.length)}  
+	#currentStep = 0;
+
+	constructor({ id, steps }) {
+		this.numberOfSteps = steps.length;
+		this.stepperElement = document.getElementById(id);
+		this.stepperElement.className = "stepper";
+		this.stepperElement.innerHTML += `<div class="stepper__bullets">
+          ${`<div class="stepper__bullet"></div>`.repeat(steps.length + 1)}  
         </div>`;
-        this.addSteps(steps);
-        this.attachEvents();
-    }
+		this.addSteps(steps);
+		this.attachEvents();
+	}
 
-    attachEvents() {
-        this.stepperElement.querySelectorAll(".stepper__field input").forEach(field => {
-            field.onkeyup = ({ key }) => {
-                if (key === "Enter" && this.currentStep < this.numberOfSteps - 1) {
-                    this.currentStep++;
-                }
-            }
-        });
+	addSteps(steps) {
+		const navigationTemplate = `<div class="stepper__previous"><img src="assets/left-arrow-icon.png" /></div>
+				<div class="stepper__next"><img src="assets/right-arrow-icon.png" /></div>`;
 
-        this.stepperElement.querySelectorAll(".stepper__bullets .stepper__bullet").forEach((bullet, i) => {
-            bullet.onclick = () => {
-                this.currentStep = i;
-            }
-        });
-    }
+		steps.forEach(({ inputs }) => {
+			const template = `
+				<div class="stepper__step">
+					${ inputs.map(input => `
+					<div class="stepper__field">
+						<input ${Object.entries(input).map(([key, value]) => `${key}="${value}"`).join` `} required />
+						<span class="stepper__placeholder">${input.placeholder}</span>
+						<span class="stepper__bottom-border"></span>
+					</div>`).join``}
+					${navigationTemplate}
+				</div>`;
+			this.stepperElement.innerHTML += template;
+		});
 
-    addSteps(steps) {
-        steps.forEach(({ inputs }) => {
-            const template = `<div class="stepper__step">
-                ${ inputs.map(input => `<div class="stepper__field">
-                <input ${ Object
-                    .entries(input)
-                    .map(([key, value]) => `${key}="${value}"`)
-                    .join` `
-                } />
-                <span class="stepper__placeholder">${input.placeholder}</span>
-                <span class="stepper__bottom-border"></span>
-                </div>`)
-                    .join``
-                }
-            </div>`;
-            this.stepperElement.innerHTML += template;
-        });
-        this.showStep();
-    }
+		this.stepperElement.innerHTML += `
+			<div class="stepper__step">
+				<button class="stepper__button">Submit</button>
+        ${navigationTemplate}
+			</div>`;
 
-    get currentStep() {
-        return this.#currentStep;
-    }
+		this.showStep();
+	}
 
-    set currentStep(value) {
-        this.#currentStep = value;
-        this.showStep();
-    }
+	attachEvents() {
+		this.stepperElement.querySelectorAll(".stepper__previous").forEach(button => {
+			button.onclick = () => {
+				if (this.currentStep > 0) {
+					this.currentStep--;
+				}
+			};
+		});
 
-    showStep() {
-        [...this.stepperElement.getElementsByClassName("stepper__step")].forEach((e, i) => {
-            if (i === this.currentStep) {
-                e.className = "stepper__step stepper--show";
-                e.querySelector("input").focus();
+		this.stepperElement.querySelectorAll(".stepper__next").forEach(button => {
+			button.onclick = () => {
+				if (this.currentStep < this.numberOfSteps) {
+					let isValid = true;
+					let password = "";
+					this.stepperElement.querySelectorAll(".stepper--show input").forEach((input, index) => {
+						if (input.type === "password" && index === 0) {
+							password = input.value;
+						}
 
-                this.stepperElement.querySelector(`.stepper__bullets .stepper__bullet:nth-of-type(${i + 1})`).className = "stepper__bullet stepper--active";
-            } else {
-                e.className = "stepper__step";
+						if (!input.validity.valid) {
+							if (isValid) {
+								input.reportValidity();
+							}
+							isValid *= false;
+						}
 
-                this.stepperElement.querySelector(`.stepper__bullets .stepper__bullet:nth-of-type(${i + 1})`).className = "stepper__bullet";
-            }
-        });
-    }
+						if (index === 1) {
+							if (input.value !== "" && input.value !== password) {
+								input.setCustomValidity("The password and its confirmation do not match.");
+								input.reportValidity();
+								isValid *= false;
+							} else if (input.value !== "" && input.value === password) {
+								input.setCustomValidity("");
+								isValid = true;
+							}
+						}
+					});
+					
+					if (isValid) {
+						this.currentStep++;
+					}
+				}
+			};
+		});
+
+		this.stepperElement.querySelector(".stepper__button").onclick = () => alert(`Thanks for Submiting!`);
+	}
+
+	get currentStep() {
+		return this.#currentStep;
+	}
+
+	set currentStep(value) {
+		this.#currentStep = value;
+		this.showStep();
+	}
+
+	showStep() {
+		this.stepperElement.querySelectorAll(".stepper__step").forEach((e, i) => {
+			if (i === this.currentStep) {
+				e.className = "stepper__step stepper--show";
+				if (e.querySelector("input")) {
+					e.querySelector("input").focus();
+				}
+				this.stepperElement.querySelector(`.stepper__bullets .stepper__bullet:nth-of-type(${i + 1})`).className = "stepper__bullet stepper--active";
+			} else {
+				e.className = "stepper__step";
+				this.stepperElement.querySelector(`.stepper__bullets .stepper__bullet:nth-of-type(${i + 1})`).className = "stepper__bullet";
+			}
+		});
+	}
 }
 
 const stepper = new Stepper({
-    id: "stepper",
-    steps: [
-        {
-            inputs: [
-                { type: "text", placeholder: "username" }
-            ]
-        },
-        {
-            inputs: [
-                { type: "email", placeholder: "email" }
-            ]
-        },
-        {
-            inputs: [
-                { type: "password", placeholder: "password" },
-                { type: "password", placeholder: "confirm password" }
-            ]
-        }
-    ]
+	id: "stepper",
+	steps: [
+		{ 
+			inputs: [
+				{ type: "text", placeholder: "username"}
+			]
+		}, {
+			inputs: [
+				{ type: "email", placeholder: "email"} 
+			]
+		}, 
+		{
+			inputs: [
+				{ type: "password", placeholder: "password" }, 
+				{ type: "password", placeholder: "confirm password" }
+			]
+		}
+	]
 });
